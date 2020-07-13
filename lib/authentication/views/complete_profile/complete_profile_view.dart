db3395a1f87f5f785d3cddafc1fdc86940211602
Fran Pitriqueo <franpitri@gmail.com>
2020-07-13 17:18:47 -0300

    add medical information tab

--- lib/authentication/views/complete_profile/complete_profile_view.dart
+++ lib/authentication/views/complete_profile/complete_profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:voluntariadoing_mobile/authentication/views/complete_profile/medical_information_tab.dart';
 import 'package:voluntariadoing_mobile/authentication/views/complete_profile/personal_information_tab.dart';
 import 'package:voluntariadoing_mobile/common/views/stepped_view.dart';
 import 'package:voluntariadoing_mobile/common/widgets/section_title.dart';
@@ -16,7 +17,7 @@ class CompleteProfileView extends StatelessWidget {
     ),
     pages: [
       PersonalInformationTab(),
-      PersonalInformationTab(),
+      MedicalInformationTab(),
       PersonalInformationTab(),
     ],
   );
