aebb3d66c2f26e89665952c7d68dc4c0b81d7123
lautaro <lm.bringas98@gmail.com>
2020-07-13 00:49:03 -0300

    add careers view tab

--- lib/authentication/views/complete_profile/complete_profile_view.dart
+++ lib/authentication/views/complete_profile/complete_profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:voluntariadoing_mobile/authentication/views/complete_profile/career_information_tab.dart';
 import 'package:voluntariadoing_mobile/authentication/views/complete_profile/personal_information_tab.dart';
 import 'package:voluntariadoing_mobile/common/views/stepped_view.dart';
 import 'package:voluntariadoing_mobile/common/widgets/section_title.dart';
@@ -17,7 +18,7 @@ class CompleteProfileView extends StatelessWidget {
     pages: [
       PersonalInformationTab(),
       PersonalInformationTab(),
-      PersonalInformationTab(),
+      CareerInformationTab(),
     ],
   );
 }
